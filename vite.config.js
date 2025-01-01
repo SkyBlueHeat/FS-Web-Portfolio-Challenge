8b8f6224bad71afeb122a3f952675256c0ea85dd
SkyBlueHeat <br.aydin@hotmail.com>
2025-01-01 16:25:17 +0300

    fix-2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  images: {
+    domains: ["images.pexels.com","miro.medium.com", "www.cnet.com"],
+  },
 })
